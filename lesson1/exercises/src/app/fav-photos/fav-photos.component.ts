423ef0273a0b11c088c2e3d19b3fce554fbd3459
Grace Brown <gracenbrown@sbcglobal.net>
2020-09-10 17:59:26 -0500

    Part 1 of Exercise Complete

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Favorite Pics';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://i.redd.it/b160jdq4crax.jpg';
+  image3 = 'https://images.theweek.com/sites/default/files/styles/tw_image_9_4/public/33577_article_full.jpg.webp?itok=qkwIO2Hb&resize=728x728';
 
   constructor() { }
 
